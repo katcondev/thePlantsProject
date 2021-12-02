eab406321ec62db7ee24ec58013a8d5ada173c79
KatDev <kat.contreras@gmail.com>
2021-12-01 16:51:57 -0800

    styled UI with brand colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,19 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      green: {
+        light: '#7b8c52',
+        DEFAULT: '#485933',
+        dark: '#1e2616',
+      },
+      gray: {
+        DEFAULT: '#f2f2f2', 
   },
   variants: {
     extend: {},
   },
-  plugins: [
-  require('@tailwindcss/typography'),
-
-  ],
-}
+  plugins: [],
+}}}
