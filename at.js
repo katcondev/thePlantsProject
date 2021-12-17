a8f7ec2ca3b21436f3a2515e3cf0e703d1de1832
Kat Contreras <katcontreras@Kats-MacBook-Pro.local>
2021-12-17 15:01:22 -0800

    JSON data

--- at.js
+++ at.js
@@ -1,19 +1,20 @@
 var Airtable = require('airtable');
 var base = new Airtable({apiKey: 'keyrnJ3ystlKHJbYv'}).base('appUEHCG0RnYm88pm');
+// const plantsdata = record.get("Name")
 
 base('plant-mood').select({
     // Selecting the first 3 records in Grid view:
-    maxRecords: 20,
+    maxRecords: 3,
     view: "Grid view"
 }).eachPage(function page(records, fetchNextPage) {
     // This function (`page`) will get called for each page of records.
 
-    // records.forEach(function(record) {
-    //     console.log('Retrieved', record.get('Name'));
-    //     console.log('Retrieved', record.get('Attachments'));
-    //     console.log('Retrieved', record.get('Scientific Name'));
-    //     console.log('Retrieved', record.get('Plant Personality'));
-    // });
+    records.forEach(function(record) {
+        console.log('Retrieved', record.get('Name'));
+        console.log('Retrieved', record.get('Attachments'));
+        console.log('Retrieved', record.get('Scientific Name'));
+        console.log('Retrieved', record.get('Plant Personality'));
+    });
     console.log(records)
     // To fetch the next page of records, call `fetchNextPage`.
     // If there are more records, `page` will get called again.
@@ -23,4 +24,7 @@ base('plant-mood').select({
 }, function done(err) {
     if (err) { console.error(err); return; }
 });
+
+
+
 
