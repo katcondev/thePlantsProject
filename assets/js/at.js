ae5ba3805d33835abd2a1c446a3565d5c0c9bb90
Kat Contreras <katcontreras@Kats-MacBook-Pro.local>
2021-12-18 18:30:25 -0800

    simplified fetch

--- assets/js/at.js
+++ assets/js/at.js
@@ -1,6 +1,7 @@
 const token = 'keyrnJ3ystlKHJbYv'
 
-fetch('https://api.airtable.com/v0/appUEHCG0RnYm88pm/plant-mood', {
+
+const info = fetch('https://api.airtable.com/v0/appUEHCG0RnYm88pm/plant-mood', {
     headers: {
         "Content-Type" : "application/json",
         "Authorization": `Bearer ${token}`
@@ -11,8 +12,5 @@ fetch('https://api.airtable.com/v0/appUEHCG0RnYm88pm/plant-mood', {
    .catch( error => console.error(error))
 
 
-
-
-
-
+document.getElementById("plants").innerHTML = `${info}`
 
