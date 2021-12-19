2ce169b6c3749990687602e0e6f350860ba17cdd
Kat Contreras <katcontreras@Kats-MacBook-Pro.local>
2021-12-19 15:24:11 -0800

    changes to database

--- assets/js/at.js
+++ assets/js/at.js
@@ -1,16 +1,48 @@
 const token = 'keyrnJ3ystlKHJbYv'
+const plantsContainer = document.getElementById("plants")
+getApi();
+
+function getApi(){
+    const requestUrl = 'https://api.airtable.com/v0/appUEHCG0RnYm88pm/plant-mood';
 
 
-const info = fetch('https://api.airtable.com/v0/appUEHCG0RnYm88pm/plant-mood', {
-    headers: {
-        "Content-Type" : "application/json",
-        "Authorization": `Bearer ${token}`
-      }
-})
+   fetch(requestUrl, {
+        headers: {
+          "Content-Type" : "application/json",
+          "Authorization": `Bearer ${token}`
+        }
+   })
    .then(response => response.json())
-   .then( json => console.log(json))
+
+   .then(function (data) {
+    //Using console.log to examine the data
+    console.log(data.records);
+    for (var i = 0; i < data.length; i++) {
+      //Creating a h3 element and a p element
+      var plantName = document.createElement('h3');
+      var plantId = document.createElement('p');
+
+      //Setting the text of the h3 element and p element.
+      plantName.textContent = data[i].records.fields.Name;
+      plantId.textContent = data[i].records.id;
+
+      //Appending the dynamically generated html to the div associated with the id="users"
+      //Append will attach the element as the bottom most child.
+      // plantsContainer.append(plantName);
+      // plantsContainer.append(plantId);
+      
+      
+      console.log(plantId.textContent)
+      
+      
+    }
+  })
    .catch( error => console.error(error))
 
+  //  document.getElementById("plants").innerHTML = `
+  //     Just a quick test! `
 
-document.getElementById("plants").innerHTML = `${info}`
+};
+
+
 
