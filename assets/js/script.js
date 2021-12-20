e866a77460881aa7d8c25c19ccc55f5b5f747cd4
Kat Contreras <katcontreras@Kats-MacBook-Pro.local>
2021-12-19 16:32:57 -0800

    changes to menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,32 @@
 var todayDate = document.querySelector('#dateToday');
 var apiKey = 'AIzaSyApqth0vbKHzQ-UhIALlWSotQ_bqSJQ6Ls'
-todayDate.textContent = moment().format("MM-Dd-YYYY")
+// todayDate.textContent = moment().format("MM-Dd-YYYY")
 // MAKE AN ARRAY with all of the videos ID's and Titles
-var plantVideos = [
-    {
-        title: "Monstera",
-        id: 'ffuqN54iono'
-    }
-]
-$(document).ready(function(){
-    function youtubCall() {
-        $.ajax({
-            type: 'GET',
-            url: 'https://www.googleapis.com/youtube/v3/videos?id=ffuqN54iono='+ apiKey,
-            type: 'json',
-            success: function(data) {
-                console.log(data)
-                // append video embedding script to HTML Page
-                // div.appendChild(embedVideo);
-            }
-        })
-    }
 
-    youtubCall();
-})
+// var plantVideos = [
+//     {
+//         title: "Monstera",
+//         id: 'ffuqN54iono'
+//     }
+// ]
+
+// $(document).ready(function(){
+//     function youtubCall() {
+//         $.ajax({
+//             type: 'GET',
+//             url: 'https://www.googleapis.com/youtube/v3/videos?id=ffuqN54iono='+ apiKey,
+//             type: 'json',
+//             success: function(data) {
+//                 console.log(data)
+//                 // append video embedding script to HTML Page
+//                 // div.appendChild(embedVideo);
+//             }
+//         })
+//     }
+
+//     youtubCall();
+// })
+
 
 // function plantVideo() {
 //     for(i=0; i<plantVideos.length; i++) {
