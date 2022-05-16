051406b9e300b71462748b503a3cfc024b440846
Kat Contreras <katcontreras@Kats-MacBook-Pro.local>
2022-05-16 14:52:00 -0700

    updated menu

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,14 +3,20 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     screens: {
-      'sm': '576px',
-      // => @media (min-width: 576px) { ... }
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
 
-      'md': '960px',
-      // => @media (min-width: 960px) { ... }
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
 
-      'lg': '1440px',
-      // => @media (min-width: 1440px) { ... }
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
     },
     extend: {
       fontFamily: {
